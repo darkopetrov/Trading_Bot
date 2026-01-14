b9d7e2c4f0df08c42dad218468d9d8875228eeef
Darko <darko.petrov1099@gmail.com>
2026-01-14 11:42:30 +0100

    nn idea

---
+++ train_nn_2.py
@@ -0,0 +1,6 @@
+# train model on trades 
+# 3 mounths interval 
+# pick random date to buy and sell 
+# lable trades as bad and good 
+# target data is 1 or 0 for trades profit or loss
+# input data is 
